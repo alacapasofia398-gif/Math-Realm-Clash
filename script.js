69c7af0b51f08c72aa7b1a5ceed57a7becd6ca30
alacapasofia398-gif <alacapasofia398@gmail.com>
2026-07-21 23:41:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,16 @@ document.getElementById('page-title').textContent = GAME_TITLE;
 document.getElementById('game-title-header').textContent = GAME_TITLE.replace(/👑/g, '').trim();
 
 // --- 1. FIREBASE CONFIGURATION ---
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBq9SQSPCxmnbYLay2lOWESY96NVYWym8k",
   authDomain: "math-real-clash.firebaseapp.com",
+  databaseURL: "https://math-real-clash-default-rtdb.firebaseio.com",
   projectId: "math-real-clash",
   storageBucket: "math-real-clash.firebasestorage.app",
   messagingSenderId: "325329128875",
   appId: "1:325329128875:web:a753cb71f7ed8ef0f0d6ee",
   measurementId: "G-57LGMVFB0H"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
